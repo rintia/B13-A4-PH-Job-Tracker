4071fb49c3039bb14db4e39e1deb3858995e0306
Rintia Islam <rintiaislam110@gmail.com>
2026-02-22 23:22:34 +0600

    total count dynamic

--- scripts/jobTracker.js
+++ scripts/jobTracker.js
@@ -1,3 +1,26 @@
+let interViewList = [];
+let rejectedList = [];
+
 let total = document.getElementById('total');
 let interviewCount = document.getElementById('interviewCount');
 let rejectedCount = document.getElementById('rejectedCount');
+
+// console.log(total, interviewCount, rejectedCount);
+
+const allCardsSection = document.getElementById('allCards');
+const mainContainer = document.querySelector('main');
+
+console.log(mainContainer);
+
+// console.log(allCardsSection.children);
+
+interViewList.push({name: 'plant 1'}, {name: 'plat 2'})
+
+function calculateCount(){
+    total.innerText = allCardsSection.children.length;
+    interviewCount.innerText = interViewList.length;
+    rejectedCount.innerText = rejectedList.length
+}
+
+
+calculateCount();
