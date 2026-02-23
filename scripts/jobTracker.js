5f35c4ec6cceeaf7f21f1a2d40a02b18e9ed1567
Rintia Islam <rintiaislam110@gmail.com>
2026-02-23 12:19:02 +0600

    button toggly style addedd

--- scripts/jobTracker.js
+++ scripts/jobTracker.js
@@ -10,11 +10,15 @@ let rejectedCount = document.getElementById('rejectedCount');
 const allCardsSection = document.getElementById('allCards');
 const mainContainer = document.querySelector('main');
 
+const allFilterBtn = document.getElementById('all-filter-btn');
+const interviewFilterBtn = document.getElementById('interview-filter-btn');
+const rejectedFilterBtn = document.getElementById('rejected-filter-btn');
+
 console.log(mainContainer);
 
 // console.log(allCardsSection.children);
 
-interViewList.push({name: 'plant 1'}, {name: 'plat 2'})
+
 
 function calculateCount(){
     total.innerText = allCardsSection.children.length;
@@ -24,3 +28,18 @@ function calculateCount(){
 
 
 calculateCount();
+
+function toggleStyle(id){
+    allFilterBtn.classList.remove('bg-blue-500', 'text-white');
+    interviewFilterBtn.classList.remove('bg-blue-500', 'text-white');
+    rejectedFilterBtn.classList.remove('bg-blue-500', 'text-white');
+
+    allFilterBtn.classList.add('bg-white', 'gray-text');
+    interviewFilterBtn.classList.add('bg-white', 'gray-text');
+    rejectedFilterBtn.classList.add('bg-white', 'gray-text');
+
+    const selected = document.getElementById(id);
+
+    selected.classList.remove('bg-white', 'gray-text');
+    selected.classList.add('bg-blue-500', 'text-white');
+}
